90bc70cc9aa1fbcdc537b7a682637d94088aae82
Jim Leitch <jim.leitch@oelan.nl>
2017-11-09 07:15:39 +0100

    checkpoint

---
+++ AlbumViewerNetCore/dockerbuild.ps1
@@ -0,0 +1,27 @@
+# publish the application first
+dotnet publish -c Release
+
+# clean up old image and any containers (running or not)
+docker stop albumviewer
+docker rm albumviewer -f 
+docker rmi westwind/albumviewer:albumviewer
+
+# create new image
+docker build -t westwind/albumviewer:albumviewer .
+
+# immediately start running the container in the background (-d) (no console)
+docker run  -it -p 5004:80 --name albumviewer  westwind/albumviewer:albumviewer 
+
+# Map host IP to a domain - so we can access local SQL server
+# $localIpAddress=((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
+#--add-host dev.west-wind.com:$localIpAddress
+
+#docker stop albumviewer
+#docker rm albumviewer
+
+# docker exec -it albumviewer  /bin/bash
+
+# # if above doesn't work
+# docker exec -it albumviewer  /bin/sh
+
+#docker push 
